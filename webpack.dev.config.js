05e1a07ab3a547b9916c25e02982fd10bcc25acc
Avishay Orpaz <avishay@magosys.com>
2018-06-11 17:24:10 +0300

    React boilerplate

---
+++ webpack.dev.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: [
+		path.join(__dirname, 'src/index.js')
+    ],
+	module: {
+		rules: [{
+			test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},{
+				test: /\.less$/,
+				loaders: ["style-loader", "css-loader", "less-loader"]
+			}
+		]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        historyApiFallback: true
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({ template: 'index.html' })
+    ]
+}
